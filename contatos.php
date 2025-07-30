dbb0b7382e8da24b6e433e7c5062f6bb89720fa6
jhammes-dev <dean.morais@hotmail.com>
2025-07-29 22:26:31 -0300

    fix: correcao de include

--- contatos.php
+++ contatos.php
@@ -1,9 +1,9 @@
 <?php
-include_once("Templates/header.php")
+include_once("templates/header.php")
 ?>
 <!-- INICIO HTML -->
    <h1>Página de Contatos</h1>
 <!-- FIM HTML -->
 <?php
-include_once("Templates/footer.php")
+include_once("templates/footer.php")
 ?>

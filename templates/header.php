4d4c627abf4520e634fd445e6d8dd32661635b26
jhammes-dev <dean.morais@hotmail.com>
2025-07-29 23:02:19 -0300

    feat: adicionando favicon ao blog

--- templates/header.php
+++ templates/header.php
@@ -10,6 +10,11 @@ include_once ("data/posts.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog 404-Buglândia</title>
+    <!-- FAVICON -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
     <!-- ESTILOS DO PROJETO -->
     <link rel="stylesheet" href= "<?= $BASE_URL ?>css/styles.css">
     <!-- GOOGLE FONTS -->
